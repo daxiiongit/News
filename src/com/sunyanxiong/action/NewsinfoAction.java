7f5b9f39d692cb93b4685621ec6004ad9e08a12f
daxiong <daxiongit@qq.com>
2016-05-02 23:15:58 +0800

    修改页面显示错误

--- src/com/sunyanxiong/action/NewsinfoAction.java
+++ src/com/sunyanxiong/action/NewsinfoAction.java
@@ -211,7 +211,7 @@ public class NewsinfoAction extends ActionSupport implements RequestAware, Sessi
 		List topicList = topicService.getAllTopics();
 		request.put("topicList", topicList);
 		
-		System.out.println("topicList:" +topicList.get(0));
+		//System.out.println("topicList:" +topicList.get(0));
 		
 		return "news_add";
 	}
@@ -223,7 +223,7 @@ public class NewsinfoAction extends ActionSupport implements RequestAware, Sessi
 		newsinfo.setAuthor(admin.getUsername());
 		newsinfo.setCreatetime(new Date());
 		
-		System.out.println("newsinfo:" + newsinfo.getId());
+	//	System.out.println("newsinfo:" + newsinfo.getId());
 		
 		newsinfoService.addNews(newsinfo);
 		return "admin";
